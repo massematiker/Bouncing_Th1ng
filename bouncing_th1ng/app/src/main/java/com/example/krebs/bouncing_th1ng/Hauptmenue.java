9d679242a09164b77603259b5dd96e030fe5b905
massematiker <38649632+massematiker@users.noreply.github.com>
2018-06-01 12:07:41 +0200

    0.1
    Funktionierende "Breakdown" Version

---
+++ bouncing_th1ng/app/src/main/java/com/example/krebs/bouncing_th1ng/Hauptmenue.java
@@ -0,0 +1,13 @@
+package com.example.krebs.bouncing_th1ng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hauptmenue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bouncing_game);
+    }
+}
