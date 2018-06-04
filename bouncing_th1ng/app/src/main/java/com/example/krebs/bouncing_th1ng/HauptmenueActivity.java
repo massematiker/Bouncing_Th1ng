0767372a355444bc3a1d0b18fd0e4b92c2115ece
massematiker <38649632+massematiker@users.noreply.github.com>
2018-06-04 14:09:34 +0200

    trash

--- bouncing_th1ng/app/src/main/java/com/example/krebs/bouncing_th1ng/HauptmenueActivity.java
+++ bouncing_th1ng/app/src/main/java/com/example/krebs/bouncing_th1ng/HauptmenueActivity.java
@@ -3,11 +3,11 @@ package com.example.krebs.bouncing_th1ng;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Hauptmenue extends AppCompatActivity {
+public class HauptmenueActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bouncing_game);
+        setContentView(R.layout.activity_hauptmenue);
     }
 }
