7cd0ae7e1068e770302dd6b453e0598200d061c7
FelixT <f.trautmann@trautmann-it.de>
2018-06-14 11:30:09 +0200

    5.3

--- bouncing_th1ng/app/src/main/java/com/example/krebs/bouncing_th1ng/BouncingGame.java
+++ bouncing_th1ng/app/src/main/java/com/example/krebs/bouncing_th1ng/BouncingGame.java
@@ -59,7 +59,9 @@ public class BouncingGame extends Activity implements SensorEventListener {
     boolean boosted2=false;
     boolean boosted3=false;
 
-    
+    MediaPlayer mediaPlayer;
+    boolean playBackgroundMusic = true;
+
     boolean touch = true;
 
     // Create the Sensors
@@ -223,10 +225,10 @@ public class BouncingGame extends Activity implements SensorEventListener {
         int gameOverDeath =-1;  //done            done
         int obstacleSound = -1;
         int paddleSound = -1;
-        int backgroundSound = -1;
 
 
-        MediaPlayer mediaPlayer;
+
+
         // The score
         int score = 0;
 
@@ -357,22 +359,25 @@ public class BouncingGame extends Activity implements SensorEventListener {
                 descriptor = assetManager.openFd("click.ogg");
                 mouse = soundPool.load(descriptor, 0);
 
-                descriptor = assetManager.openFd("background8bit.ogg");
-                backgroundSound = soundPool.load(descriptor, 0);
 
             }catch(IOException e){
                 // Print an error message to the console
                 Log.e("error", "failed to load sound files");
             }
 
-         //   soundPool.setLoop(backgroundSound,99); //TODO
-       /*     mediaPlayer = MediaPlayer.create(getContext(),R.raw.background8bit);
-            mediaPlayer.setVolume(0.5f,0.5f );
-            mediaPlayer.start();
+
+            if(playBackgroundMusic){
+                mediaPlayer = MediaPlayer.create(getContext(),R.raw.background8bit);
+                mediaPlayer.setVolume(0.4f,0.4f );
+                mediaPlayer.setLooping(true);
+                mediaPlayer.start();
+            }
+
 
             createObstaclesAndRestart();
-*/
+
         }
+
 
         /**
          * create all obstacles new and start a new game
@@ -568,6 +573,7 @@ public class BouncingGame extends Activity implements SensorEventListener {
 
             // Move the paddle if required
             paddle.update(fps);
+
 
             // Check for obstacle colliding
             for(int i = 0; i < numObstacles; i++){
@@ -1144,6 +1150,9 @@ public class BouncingGame extends Activity implements SensorEventListener {
             } catch (InterruptedException e) {
                 Log.e("Error:", "joining thread");
             }
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
 
         }
 
@@ -1155,6 +1164,9 @@ public class BouncingGame extends Activity implements SensorEventListener {
             playing = true;
             gameThread = new Thread(this);
             gameThread.start();
+            if (playBackgroundMusic && !mediaPlayer.isPlaying()) {
+                mediaPlayer.start();
+            }
         }
 
 
